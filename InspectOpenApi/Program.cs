cf9c49d0c9df925748ba83ca670b1603f0c083a1
BaoVuong150 <baovuong150@users.noreply.github.com>
2026-05-21 21:39:22 +0700

    chore: Remove unused InspectOpenApi test project

--- InspectOpenApi/Program.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using Microsoft.OpenApi;
-
-var req = new OpenApiSecurityRequirement
-{
-    [new OpenApiSecuritySchemeReference("Bearer")] = new List<string>()
-};
-Console.WriteLine("OK");
